10aad071c0b5efd3dad411de326f204b01bb9630
cherry4xo <1jlaketuk1@gmail.com>
2023-10-07 18:34:45 +0300

    autocommit

--- procedural/homework/5/4.cpp
+++ procedural/homework/5/4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <typeinfo>
 
 class Book
 {
@@ -54,26 +55,29 @@ int main(int argc, const char** argv)
 
     State state;
 
-    u_int8_t command;
+    int command;
+    std::string buff;
     std::cout << "1. Add book\n2. Delete book by title and release year\n3. Find book by title\n4. Exit\n";
     std::cout << "Enter the command: ";
-    std::cin >> command;
+    std::getline(std::cin, buff);
+    command = std::stoi(buff);
     switch (command)
     {
     case 1:
-        state = State::exit;
-        break;
-    case 2:
         state = State::addBook;
         break;
-    case 3:
+    case 2:
         state = State::deleteBook;
         break;
-    case 4:
+    case 3:
         state = State::findBook;
+        break;
+    case 4:
+        state = State::exit;
         break;
     default:
         std::cout << "Incorrect command number\n";
+        state = State::exit;
         break;
     }
 
@@ -81,36 +85,45 @@ int main(int argc, const char** argv)
     {
         std::string firstName, secondName, middleName, title;
         u_int16_t year;
+        bool flag = 0;
         switch (state)
         {
         case State::exit:
-            return 0;
+            break;
         case State::addBook:
             std::cout << "First name of author: ";
-            std::cin >> firstName;
+            std::getline(std::cin, firstName);
             std::cout << "Second name of author: ";
-            std::cin >> secondName;
+            std::getline(std::cin, secondName);
             std::cout << "Middle name of author: ";
-            std::cin >> middleName;
+            std::getline(std::cin, middleName);
             std::cout << "Title name: ";
-            std::cin >> title;
+            std::getline(std::cin, title);
             std::cout << "Release year: ";
-            std::cin >> year;
+            std::getline(std::cin, buff);
+            year = std::stoi(buff);
             library.push_back(Book(firstName, secondName, middleName, title, year));
             std::cout << "Book " << library.back().getTitle() << " added successfully\n";
             break;
         case State::deleteBook:
             std::cout << "Title name: ";
-            std::cin >> title;
+            std::getline(std::cin, title);
             std::cout << "Title release year: ";
-            std::cin >> year;
+            std::getline(std::cin, buff);
+            year = std::stoi(buff);
+            flag = 0;
             for(auto i = library.begin(); i != library.end(); ++i)
                 if (i->getTitle() == title && i->getReleaseYear() == year)
+                {
                     library.erase(i);
+                    flag = 1;
+                }
+            if (flag) std::cout << "Book was deleted successfully\n";
+            else std::cout << "Cannot find a book with that data\n";
             break;
         case State::findBook:
             std::cout << "Title name: ";
-            std::cin >> title;
+            std::getline(std::cin, title);
             for (auto i = library.begin(); i != library.end(); ++i)
                 if (i->getTitle().find(title) != std::string::npos)
                 {
@@ -122,10 +135,29 @@ int main(int argc, const char** argv)
             std::cout << "Incorrect command number\n";
             break;
         }
-        u_int8_t command;
-        std::cout << "1. Add book\n2. Delete book by title and release year\n3. Find book by title and release year\n4. Exit\n";
+        std::cout << "1. Add book\n2. Delete book by title and release year\n3. Find book by title\n4. Exit\n";
         std::cout << "Enter the command: ";
-        std::cin >> command;
+        std::getline(std::cin, buff);
+        command = std::stoi(buff);
+        switch (command)
+        {
+        case 1:
+            state = State::addBook;
+            break;
+        case 2:
+            state = State::deleteBook;
+            break;
+        case 3:
+            state = State::findBook;
+            break;
+        case 4:
+            state = State::exit;
+            break;
+        default:
+            std::cout << "Incorrect command number\n";
+            state = State::exit;
+            break;
+        }
     }
 
     return 0;
